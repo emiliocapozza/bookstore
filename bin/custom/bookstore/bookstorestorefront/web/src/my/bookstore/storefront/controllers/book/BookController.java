a3bb8d24381351583bfb2f84f6e5608203e6ee77
CAPOZZAEmilio <emilio.capozza@soprasteria.com>
2021-12-15 17:53:58 +0100

    modified getMethod with requestParam

--- bin/custom/bookstore/bookstorestorefront/web/src/my/bookstore/storefront/controllers/book/BookController.java
+++ bin/custom/bookstore/bookstorestorefront/web/src/my/bookstore/storefront/controllers/book/BookController.java
@@ -10,6 +10,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import my.bookstore.facades.book.impl.DefaultBookFacade;
@@ -29,9 +30,9 @@ public class BookController
 
 	@RequestMapping(value = "/mostRentedBook", method = RequestMethod.GET)
 	@ResponseBody
-	public List<BookData> printMostRentedBooks()
+	public List<BookData> printMostRentedBooks(@RequestParam(value = "numberOfBooks") final int numberOfBooks)
 	{
-		final int numberOfBooks = 5;
+		//final int numberOfBooks = 5;
 		return defaultBookFacade.returnMostRentedBooks(numberOfBooks);
 	}
 
